26f23ef10f0fb7358216a9aee4bb1d773eaa72c5
JhonnyGuillen <jhonnyguillen26@gmail.com>
2021-04-06 21:55:22 -0600

    intento de que funcione Frogger sin arranque, no se pudo pero para que vean el trabajo honesto

---
+++ prueba.asm
@@ -0,0 +1,98 @@
+.data
+
+  pepegaWin db "⣿⣿⣿⣿⣏⠉⠙⠋⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿", 0xA,  "⣿⣿⣿⠟⠋⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠈⠀⠀⣸⣿⣿⣿", 0xA,  "⣿⡟⠁⠀⢠⣶⡷⠶⠚⠿⠿⣿⣿⣿⡿⠛⠋⠉⠉⠉⠻⡃⠀⠀⠀⠈⠙⠻⣿⣿", 0xA,  "⣿⠁⠀⠠⠟⠉⠀⣀⠤⠤⠤⢤⣉⠉⢀⣠⠤⠒⢒⠶⠚⠓⠀⠈⠓⢦⡀⠀⠈⣿", 0xA,  "⡿⠀⠀⠀⠀⠴⠋⠀⠀⢀⡠⠤⠌⣀⣀⡀⠄⠊⠁⣠⠄⡀⠈⠻⣶⣄⢻⠀⠀⢸", 0xA,  "⡇⠀⠀⠀⠀⠀⢀⡠⠞⢁⣤⢠⡄⢨⠹⣿⣷⡄⣾⣿⠀⠄⠘⠂⣿⣿⢣⠀⠀⢸", 0xA,  "⡇⠀⠀⠀⠀⠛⠉⠀⢴⣿⣿⠀⠂⠈⠀⠸⠟⠘⠻⠿⠆⠀⠀⠀⠈⡰⡿⠀⠀⢸", 0xA,   "⡇⠀⠀⠀⠀⠀⣀⣀⣀⣉⣉⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣤⣴⡿⢧⡇⠀⣾", 0xA,  "⡇⠀⠀⠀⠀⠸⣿⣯⣙⣛⡛⠿⠿⠿⠿⠿⠿⠿⠟⢛⣛⣋⣭⣥⡖⡿⠁⠀⢠⣿", 0xA,  "⣧⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠛⠛⠛⠛⠛⠛⠛⠉⠉⠉⠀⠀⠀⠀⢀⣾⣿", 0xA,  "⣿⡀⠀⠀⠐⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⡶⣠⣴⣿⣿⣿", 0xA,  "⣿⣷⠀⠀⠀⠀⣿⣿⣶⣤⣤⣤⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⣿⣿⣷⣌⠻⣿⣿⣿", 0xA,  "⣿⡏⣾⣦⣄⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢹⣿⣿", 0xA,  "⣿⣇⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣼⣿⣿", 0xA,  "⣿⣿⣷⣶⣯⣭⣭⣽⣟⣛⣛⣛⣛⣿⣿⣛⣛⣛⣛⣛⣫⣭⣭⣿⣶⣾⣿⣿⣿⣿", 0xA , "⣿⣿⣿⣿⣿⣿ I hate assambly ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇", 0xA
+  lenPepeWin equ $-pepegaWin
+
+  pepegaFail db "⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠙⠿⠛⠉⠁⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿", 0xA, "⣿⣿⣿⡿⠁⠀⢠⠤⠖⠒⠒⠲⢬⣇⣀⠤⠠⢄⣀⡹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿", 0xA, "⣿⣿⡯⠁⠀⠀⠀⣠⣴⡶⠖⠲⠾⣢⡠⣴⡲⠶⠶⣶⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿", 0xA, "⡿⠋⠀⠀⠀⠀⢞⣛⠋⠁⠀⠀⠀⠀⠀⡧⠀⠀⠀⠀⠀⠀⡀⣿⣿⣿⣿⣿⣿⣿", 0xA, "⠁⠀⠀⠀⠀⠀⠀⢹⡿⢶⣶⣶⣶⣶⣾⠿⠷⢶⡶⠶⠶⠟⣿⣿⣿⣿⣿⣿⣿⣿", 0xA, "⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢠⠴⠞⠋⠀⠐⠲⠼⣷⠖⠲⣿⣿⣿⣿⣿⣿⣿⣿⣿", 0xA, "⠀⠀⠀⠀⠀⠀⣠⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⢻⣿⣿⡿⢃⢰⡍⠝⣿", 0xA, "⠀⠀⠀⠀⠀⠐⣵⣖⣓⡲⠦⣤⣤⣄⣀⣀⣀⣀⣀⣨⡥⢆⣏⢨⠻⡘⣆⣷⡸⣸", 0xA, "⠀⠀⠀⠀⠐⢄⡈⠉⠉⠉⠙⠒⢂⡄⡀⠀⡄⠶⠶⠶⣂⡿⢻⢸⣷⣾⣿⢟⣵⣿", 0xA, "⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀  ⢦⡀⢸⣤⣇⢠⡇⢰⠐⣿⡟⢰⣿⣿⠟⣫⣾⣿⡇", 0xA, "⣿⣿⣿⣶⣶⣤⣤⣄⣀⣀⣀⣙⠻⢿⢿⣿⠖⢫⣾⣿⣷⢸⣿⣿⡇⣿⣿⣿⣿⣿", 0xA, "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⡆⣿⣦⡙⢿⣿⣏⣿⣿⣷⠻⣿⣿⣿⣿", 0xA, "⣿⣿⣿⣿⣎⠻⣿⣿⣿⣿⣿⡇⣿⣿⣿⢁⣿⣿⣿⣦⡙⣿⢻⣿⣿⡇⢿⣿⣿⣿", 0xA, "⡝⣿⣿⣿⣿⣷⣜⢿⣿⣿⡿⢰⣿⣿⣿⢘⢿⣿⣿⣿⣿⢀⣿⣿⣿⡇⣾⣿⣿⣿", 0xA, "⣿⡌⢿⣿⣿⣿⣿⣧⡙⢿⢣⣿⣿⣿⡏⣼⣧⣌⠻⢿⢣⣿⣿⣿⣿⢇⣿⣿⣿⣿", 0xA, "⣿⣿⣿⣿⣿⣿ I hate assambly ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇", 0xA
+  lenPepeFail equ $-pepegaFail
+
+  carretera db '0','0','3','0','0',0xA, '0','1','0','1','0',0xA,'1','0','1','0','1',0xA,'0','1','0','1','0',0xA,'0','0','4','0','0',0xA
+  lenCarretera equ $-carretera
+
+  fila db "2"
+  lenFila equ $-fila
+
+  carro db "1"
+  lenCarro equ $-carro
+
+  libre db "0"
+  lenLibre equ $-libre
+
+  sapito db "3"
+  lenSapito equ $-sapito
+
+  meta db "4"
+  lenMeta equ $-meta
+
+  carril db "1"
+  lenCarril equ $-carril
+
+  direccion  db 0,0,0,0,0
+  la equ $-direccion
+
+
+
+.bss
+  res resb 2
+
+.text
+
+global _start
+
+_start:
+
+imprimirCarretera:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, carretera
+  mov edx, lenCarretera
+  int 0x80
+
+  mov esi, direccion
+  mov edi, 0
+
+  ;Leer de teclado
+lecturaTeclado:
+  mov eax, 3
+  mov ebx, 0
+  mov ecx, direccion
+  mov edx, 2
+  int 0x80
+
+  mov al, [res]
+  sub al, '0'
+  ; almacenamiento de entrada de teclado
+  mov [esi], al
+  add esi, 1
+  add edi, 1
+
+  ;cmp edi, la
+  ;jb lecturaTeclado
+  
+  mov [carretera+fila*carril], 
+  mov cl, [carretera+fila]
+
+  ;cmp cl, libre
+  ;je imprimirGane
+  ;jmp imprimirGane
+
+salir:
+  mov eax, 1
+  xor ebx, ebx
+  int 0x80
+
+imprimirGane:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, pepegaWin
+  mov edx, lenPepeWin
+  int 0x80
+  jmp salir
+
+imprimirLoPerder:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, pepegaFail
+  mov edx, lenPepeFail
+  int 0x80
+  jmp salir
